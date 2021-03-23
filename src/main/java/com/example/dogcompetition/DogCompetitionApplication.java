1bbef29751677222eb062a68c9b8b2547c1ecac8
Lidia Belyaeva <lvcaciag@gmail.com>
2021-03-23 14:59:40 +0200

    Initial commit

---
+++ src/main/java/com/example/dogcompetition/DogCompetitionApplication.java
@@ -0,0 +1,13 @@
+package com.example.dogcompetition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogCompetitionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DogCompetitionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dogcompetition/DogCompetitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dogcompetition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogCompetitionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
