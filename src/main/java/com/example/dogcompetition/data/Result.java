b9961ba342617b3b68b067aa1d974d460d8a45e7
Lidia Belyaeva <lvcaciag@gmail.com>
2021-04-12 14:26:09 +0300

    SQL file added, Results class added, changed in html - better representing of tables

---
+++ src/main/java/com/example/dogcompetition/data/Result.java
@@ -0,0 +1,39 @@
+package com.example.dogcompetition.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "results")
+public class Result {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "result_id")
+    private Integer id;
+
+    @Column (name = "time")
+    private Double time;
+    @Column (name = "faults")
+    private Integer faults;
+    @Column (name = "refusals")
+    private Integer refusals;
+    @Column (name = "disq")
+    private String disq;    @Column (name = "speed")
+    private Double speed;
+    @Column (name = "mistakes")
+    private Integer mistakes;
+    @Column (name = "time_faults")
+    private Double timeFaults;
+    @Column (name = "total_faults")
+    private Double totalFaults;
+
+    @OneToOne(cascade = CascadeType.DETACH, fetch = FetchType.EAGER)
+    @JoinColumn(name="dog_result_id", referencedColumnName = "dog_id", nullable = false)
+    private Dog dog;
+}
