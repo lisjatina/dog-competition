1593864e3efc040cee9e09e6c8dc1109f2bfb31e
ElizaEB <elizabete.bicane@gmail.com>
2021-04-15 16:07:55 +0300

    result page showing with partial results

--- src/main/resources/static/script/index.js
+++ src/main/resources/static/script/index.js
@@ -1,20 +1,44 @@
 var columnDefs = [
-    {headerName: "Place", field: "place", filter: true, sortable: true},
-    {headerName: "Dog", field: "dog", filter: true, sortable: true},
-    {headerName: "Handler", field: "handler", filter: true, sortable: true},
+    {headerName: "Place", field: "id", filter: true, sortable: true},
+    {headerName: "Dog", valueGetter: "dog", rowGroupIndex: 1, filter: true, sortable: true},
+    {headerName: "Handler", field: "dog", filter: true, sortable: true},
     {headerName: "Time (s)", field: "time", filter: true, sortable: true},
     {headerName: "Speed (m/s)", field: "speed", filter: true, sortable: true},
     {headerName: "Mistakes", field: "mistakes", filter: true, sortable: true},
-    {headerName: "Time faults", field: "time_faults", filter: true, sortable: true},
-    {headerName: "Total faults", field: "total_faults", filter: true, sortable: true},
+    {headerName: "Time faults", field: "timeFaults", filter: true, sortable: true},
+    {headerName: "Total faults", field: "totalFaults", filter: true, sortable: true},
     {headerName: "Disq", field: "disq", filter: true, sortable: true}
 ];
+
+
+// var rowDefs = [
+//     {headerName: "Place", field: "place", filter: true, sortable: true},
+//     {headerName: "Dog", field: "dog", filter: true, sortable: true},
+//     {headerName: "Handler", field: "handler", filter: true, sortable: true},
+//     {headerName: "Time (s)", field: "time", filter: true, sortable: true},
+//     {headerName: "Speed (m/s)", field: "speed", filter: true, sortable: true},
+//     {headerName: "Mistakes", field: "mistakes", filter: true, sortable: true},
+//     {headerName: "Time faults", field: "timeFaults", filter: true, sortable: true},
+//     {headerName: "Total faults", field: "totalFaults", filter: true, sortable: true},
+//     {headerName: "Disq", field: "disq", filter: true, sortable: true}
+// ];
 
 // let the grid know which columns and what data to use
 var gridOptions = {
     columnDefs: columnDefs,
-    // rowData: rowData
+    rowSelection: 'single',
+   onSelectionChanged: onSelectionChanged
 };
+
+// // let the grid know which columns and what data to use
+// var gridOptions = {
+//     columnDefs: columnDefs,
+//     // rowData: rowData
+// };
+
+// agGrid.simpleHttpRequest({url: 'http://localhost:8080/api/res'}).then(data => {
+//     gridOptions.api.setRowData(data);
+// });
 
 // setup the grid after the page has finished loading
 document.addEventListener('DOMContentLoaded', function () {
@@ -23,49 +47,54 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-// function onSelectionChanged() {
-//     var selectedRows = gridOptions.api.getSelectedRows();
-//
-//     if (selectedRows.length === 1) {
-//         $('#selectedResult').text(selectedRows[0].time);
-//
-//         $('#btnEdit').prop("disabled", false);
-//     }
-//
-//     // document.querySelector('#selectedRows').innerHTML =
-//     //     selectedRows.length === 1 ? selectedRows[0].athlete : '';
-// }
+function onSelectionChanged() {
+    var selectedRows = gridOptions.api.getSelectedRows();
+
+    if (selectedRows.length === 1) {
+        $('#selectedResult').text(selectedRows[0].time);
+
+        $('#btnEdit').prop("disabled", false);
+    }
+
+    // document.querySelector('#selectedRows').innerHTML =
+    //     selectedRows.length === 1 ? selectedRows[0].athlete : '';
+}
 
 //
-// $(function () {
-//     var gridDiv = document.querySelector('#resultList');
-//     new agGrid.Grid(gridDiv, gridOptions);
-//
-//     agGrid.simpleHttpRequest({url: 'http://localhost:8080/results'}).then(function (data) {
-//         gridOptions.api.setRowData(data);
+$(function () {
+    var gridDiv = document.querySelector('#resultList');
+    new agGrid.Grid(gridDiv, gridOptions);
+
+    agGrid.simpleHttpRequest({url: 'http://localhost:8080/api/res'}).then(function (data) {
+        gridOptions.api.setRowData(data);
+    });
+});
+
+
 
 
         //$('#loader').css('display','none');
     // });
 //
-//     $('#btnEdit').bind('click', function () {
-//         var selectedRows = gridOptions.api.getSelectedRows();
-//         if (selectedRows.length === 1) {
-//             console.log(selectedRows[0].id);
-//
-//             let editModal = new bootstrap.Modal(document.getElementById('resultEditModal'));
-//             editModal.show();
-//
-//             let settings = {
-//                 // "timeout": 3000,
-//                 "async": true,
-//                 "crossDomain": true,
-//                 "url": "http://localhost:8080/results/" + selectedRows[0].id,
-//                 "method": "GET",
-//                 "headers": {
-//                     "cache-control": "no-cache"
-//                 }
-//             }
+    $('#btnEdit').bind('click', function () {
+        var selectedRows = gridOptions.api.getSelectedRows();
+        if (selectedRows.length === 1) {
+            console.log(selectedRows[0].id);
+
+            let editModal = new bootstrap.Modal(document.getElementById('resultEditModal'));
+            editModal.show();
+
+            let settings = {
+                // "timeout": 3000,
+                "async": true,
+                "crossDomain": true,
+                "url": "http://localhost:8080/results/" + selectedRows[0].id,
+                "method": "GET",
+                "headers": {
+                    "cache-control": "no-cache"
+                }
+            }
+
 //
 //             $.ajax(settings).done(function (response) {
 //
@@ -75,8 +104,8 @@ document.addEventListener('DOMContentLoaded', function () {
 //
 //                 console.log(response);
 //             });
-//         }
-//     });
+        }
+    });
 
 
 
